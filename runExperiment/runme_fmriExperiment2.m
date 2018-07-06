df1902fd091912c35a2274a15780ad13929c717f
Jingyang Zhou <jyz205@nyu.edu>
2018-07-06 11:33:49 -0400

    adding more experimental code

---
+++ runExperiment/runme_fmriExperiment2.m
@@ -0,0 +1,24 @@
+% total 262.5 seconds, 4.375 minutes, 175 TRs
+
+% Initial 8 TRS, 54 trials, each trial 4.5s, 5 TRs at the end
+
+
+
+params = retCreateDefaultGUIParams;
+params.fixation      = 'digits';
+params.skipSyncTests = false;
+params.triggerKey    = '`';
+params.calibration   = 'CBI_NYU_projector';
+params.experiment    = 'experiment from file';
+loadMatrix_str       = 'temporalExp26.mat';
+params.loadMatrix    = loadMatrix_str;
+params.modality      = 'fMRI';
+params.devices       = 'External: 5'; 
+ret(params)
+
+
+%%
+
+% data = '~/Desktop/20160118t133052.mat';
+% a =load(data);
+% [m, result]= analyze_key_presses(a, 1, [], 1)
